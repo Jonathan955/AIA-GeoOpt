d4416006d3f5dbcb30e9a651eed60e94f6ff63c3
Jonathan955 <77129124+Jonathan955@users.noreply.github.com>
2021-05-11 00:02:14 +0200

    Update a02.py

--- a02/a02.py
+++ a02/a02.py
@@ -1,1 +1,43 @@
-a02
+"""Provides a scripting component.
+    Inputs:
+        m: a mesh
+        s: sun vector
+    Output:
+        a: List of Vectors
+        b: List of Points
+        c: list of angles
+        d: exploded mesh
+        """
+        
+import Rhino.Geometry as rg
+
+#1.
+#compute face normals using rg.Mesh.FaceNormals.ComputeFaceNormals()
+#output the vectors to a
+
+a = faceNormals
+
+#2.
+#get the centers of each faces using rg.Mesh.Faces.GetFaceCenter()
+#store the centers into a list called centers 
+#output that list to b
+
+b = centers
+
+#3.
+#calculate the angle between the sun and each FaceNormal using rg.Vector3d.VectorAngle()
+#store the angles in a list called angleList and output it to c
+
+c = angleList
+
+
+#4. explode the mesh - convert each face of the mesh into a mesh
+#for this, you have to first copy the mesh using rg.Mesh.Duplicate()
+#then iterate through each face of the copy, extract it using rg.Mesh.ExtractFaces
+#and store the result into a list called exploded in output d
+
+d = exploded
+
+#after here, your task is to apply a transformation to each face of the mesh
+#the transformation should correspond to the angle value that corresponds that face to it... 
+#the result should be a mesh that responds to the sun position... its up to you!

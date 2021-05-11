162a57c7a6dc7dc28215d42c49dd33d2f7da24c5
Jonathan955 <77129124+Jonathan955@users.noreply.github.com>
2021-05-11 23:35:30 +0200

    Update

--- a02/a02.py
+++ a02/a02.py
@@ -1,3 +1,6 @@
+# Assigment 02
+# Jonathan Hernandez
+
 """Provides a scripting component.
     Inputs:
         m: a mesh
@@ -10,34 +13,58 @@
         """
         
 import Rhino.Geometry as rg
+import ghpythonlib.treehelpers as th
+import math
+import ghpythonlib.components as ghc
 
+#
 #1.
 #compute face normals using rg.Mesh.FaceNormals.ComputeFaceNormals()
 #output the vectors to a
+#
 
-a = faceNormals
+m.FaceNormals.ComputeFaceNormals
+a = m.FaceNormals
+#print(a)
 
+#
 #2.
 #get the centers of each faces using rg.Mesh.Faces.GetFaceCenter()
 #store the centers into a list called centers 
 #output that list to b
+#
 
-b = centers
+centers = []
+for i in range(m.Faces.Count):
+    cen = m.Faces.GetFaceCenter(i)
+    centers.append(cen)
+    b = centers
+#print(b)
 
+#
 #3.
 #calculate the angle between the sun and each FaceNormal using rg.Vector3d.VectorAngle()
 #store the angles in a list called angleList and output it to c
+#
 
-c = angleList
+angleList = []
+for i in a:
+    ang = rg.Vector3d.VectorAngle(s,i)
+    angleList.append(ang)
+    
+#c = angleList
+print(c)
 
-
+#
 #4. explode the mesh - convert each face of the mesh into a mesh
 #for this, you have to first copy the mesh using rg.Mesh.Duplicate()
 #then iterate through each face of the copy, extract it using rg.Mesh.ExtractFaces
 #and store the result into a list called exploded in output d
+#
 
+md = rg.Mesh.Duplicate(m)
+for i in range(len(md.faces)):
+    mef = md.Faces.ExtractFaces([0])
+    exploded.append(mef)
 d = exploded
-
-#after here, your task is to apply a transformation to each face of the mesh
-#the transformation should correspond to the angle value that corresponds that face to it... 
-#the result should be a mesh that responds to the sun position... its up to you!
+#print(d)
